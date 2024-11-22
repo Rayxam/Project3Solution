996f4e9bb8fe90cfd669241952661c898ef2f6e9
alexandr <Rayxam@gmail.com>
2024-11-22 11:46:49 +0300

    Initial commit

---
+++ Project3/src/main/java/ru/alishev/springcourse/project3/util/MeasurementException.java
@@ -0,0 +1,7 @@
+package ru.alishev.springcourse.project3.util;
+
+public class MeasurementException extends RuntimeException {
+    public MeasurementException(String msg) {
+        super(msg);
+    }
+}
